f9568daa875c1a5134f13d563efdd03f54323a2d
amirkhosrovosughi <amirkhosro.vosughi@gmail.com>
2021-01-11 18:06:51 -0800

    Map generation -ver1

--- tiers_drone_racing/src/image_pub_sub.py
+++ tiers_drone_racing/src/image_pub_sub.py
@@ -15,6 +15,10 @@ pub = rospy.Publisher('/Processed_imag/rgb_image', Image, queue_size=1)
 edited_imag = np.zeros((2, 2, 2))
 pub2 = rospy.Publisher('/Processed_imag/center', Int16MultiArray, queue_size=1)
 pub3 = rospy.Publisher('/Processed_imag/line_edge', Int16MultiArray, queue_size=1)
+
+pub4 = rospy.Publisher('/Processed_imag/mask_circle', Image, queue_size=1)
+pub5 = rospy.Publisher('/Processed_imag/mask_line', Image, queue_size=1)
+
 center_coordinate = Int16MultiArray()
 center_coordinate.data = []
 edge_coordinate = Int16MultiArray()
@@ -36,8 +40,8 @@ def process_contours(binary_image, cv_imag1, contours, type):
             if type == "circle":
                 perimeter= cv2.arcLength(c, True)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                cv2.drawContours(rgb_image, [c], -1, (226,198,85), 1)
-                cv2.drawContours(black_image, [c], -1, (226,198,85), 1)
+                cv2.drawContours(rgb_image, [c], -1, (226,198,85), 2)
+                cv2.drawContours(black_image, [c], -1, (226,198,85), 2)
                 cx, cy = get_contour_center(c)
                 cv2.circle(rgb_image, (cx,cy),(int)(radius),(255,97,97),1)
                 cv2.circle(black_image, (cx,cy),(int)(radius),(255,97,97),1)
@@ -54,8 +58,8 @@ def process_contours(binary_image, cv_imag1, contours, type):
             elif type == "line":
                 perimeter= cv2.arcLength(c, True)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                cv2.drawContours(rgb_image, [c], -1, (150,250,150), 1)
-                cv2.drawContours(black_image, [c], -1, (150,250,150), 1)
+                cv2.drawContours(rgb_image, [c], -1, (150,250,150), 2)
+                cv2.drawContours(black_image, [c], -1, (150,250,150), 2)
                 #cx, cy = get_contour_center(c)
                 #cv2.circle(rgb_image, (cx,cy),(int)(radius),(0,0,255),1)
                 #cv2.circle(black_image, (cx,cy),(int)(radius),(0,0,255),1)
@@ -72,9 +76,11 @@ def process_contours(binary_image, cv_imag1, contours, type):
             elif type == "line_edge":
                 perimeter= cv2.arcLength(c, True)
                 ((x, y), radius) = cv2.minEnclosingCircle(c)
-                cv2.drawContours(rgb_image, [c], -1, (226,198,85), 1)
-                cv2.drawContours(black_image, [c], -1, (226,198,85), 1)
+                #cv2.drawContours(rgb_image, [c], -1, (226,198,85), 1)
+                #cv2.drawContours(black_image, [c], -1, (226,198,85), 1)
                 cx, cy = get_contour_center(c)
+                cv2.circle(rgb_image, (cx,cy),(int)(radius),(10,10,10),1)
+                cv2.circle(black_image, (cx,cy),(int)(radius),(10,10,10),1)
                 cv2.circle(black_image, (cx,cy), radius=0, color=(255,255, 255), thickness=5)
                 cv2.circle(rgb_image, (cx,cy), radius=0, color=(255,255, 255), thickness=5)
                 x_coordinate = math.floor(cx - cols/2)
@@ -82,7 +88,7 @@ def process_contours(binary_image, cv_imag1, contours, type):
                 print ("Line Edge is in {} and {}".format(x_coordinate, y_coordinate))
                 edge_coordinate.data = [x_coordinate, y_coordinate]
                 pub3.publish(edge_coordinate)
-                cv2.imshow("RGB Image Contours - Circle",rgb_image)
+                cv2.imshow("RGB Image Contours - line edge",rgb_image)
             else:
                 print('Undefined object code!')
 
@@ -122,6 +128,9 @@ def find_circle(cv_imag,lower_band,upper_band):
     #cv2.imshow("hsv image",hsv)
     # masking
     mask = cv2.inRange(hsv, lower_band, upper_band)
+    #mask_ros = bridge.cv2_to_imgmsg(mask, encoding="bgr8")
+    mask_ros = bridge.cv2_to_imgmsg(mask, encoding="mono8")
+    pub4.publish(mask_ros)
     #cv2.imshow("mask image", mask);
     # countor
     contours = getContours(mask)
@@ -140,14 +149,18 @@ def find_line(cv_imag,processed_imag,lower_band,upper_band,type="line"):
     #cv2.imshow("CV image 2",cv_imag)
     # masking
     mask = cv2.inRange(hsv, lower_band, upper_band)
-    mask2 = Fill_the_center(mask,width=50)
+    mask_ros = bridge.cv2_to_imgmsg(mask, encoding="mono8")
+    pub5.publish(mask_ros)
 
+    mask2 = cv2.inRange(hsv, lower_band, upper_band)
+    mask2 = Fill_the_center(mask2,width=25)
+    #cv2.imshow("mask original after feeling",mask)
     # countor
     contours = getContours(mask)
     contours2 = getContours(mask2)
     #draw_contours(cv_imag, contours,"RGB Contours")
     rgb_image, black_image = process_contours(mask, processed_imag, contours,type="line")
-    rgb_image, black_image = process_contours(mask, cv_imag, contours2,type="line_edge")
+    rgb_image, black_image = process_contours(mask2, rgb_image, contours2,type="line_edge")
     return rgb_image
 
 
@@ -158,12 +171,13 @@ def image_callback(ros_image):
   #convert ros_image into an opencv-compatible image
   try:
     cv_image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
+    cv_image2 = bridge.imgmsg_to_cv2(ros_image, "bgr8") #==> Add it becuase the orginal picture change when it pass to a function
+    cv_image3 = bridge.imgmsg_to_cv2(ros_image, "bgr8") #==> cv_image, cv_image2, cv_image3 are same copies
   except CvBridgeError as e:
       print(e)
-  #from now on, you can work exactly like with opencv
+  #show the image
   cv2.imshow("Raw Camera", cv_image)
   cv2.waitKey(3)
-  cv_image1 = cv_image
   # Dectecting the objects in the world
   #finding circle
   lower_band =(60, 150, 100)
@@ -173,10 +187,11 @@ def image_callback(ros_image):
   # finding the line
   lower_band =(0, 80, 100)
   upper_band = (30, 255, 255)
-  cv2.imshow("Raw Camera 2", cv_image1)
-  processed_imag=find_line(cv_image1, processed_imag,lower_band,upper_band)
-  # the line trajectory and, is published inside the above function
+  cv2.imshow("Raw Camera 2", cv_image2)
+  processed_imag=find_line(cv_image2, processed_imag,lower_band,upper_band)
+  # the line ends are published inside the above function
   # Publishing as a topic ==> find out best practice for publishing
+  cv2.imshow("Final Processed Imag", processed_imag)
   edited_imag = bridge.cv2_to_imgmsg(processed_imag, encoding="bgr8")
   #pub = rospy.Publisher('edited_imag/imag_with_text', Image, queue_size=1)
   pub.publish(edited_imag)
--- tiers_drone_racing/src/opencv_test.py
+++ tiers_drone_racing/src/opencv_test.py
@@ -51,10 +51,46 @@ def Fill_the_center(mask,width):
     return img2
 
 
+
+import cv2
+
+def rotate_image(mat, angle):
+    """
+    Rotates an image (angle in degrees) and expands image to avoid cropping
+    """
+
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    # rotation calculates the cos and sin, taking absolutes of those.
+    abs_cos = abs(rotation_mat[0,0])
+    abs_sin = abs(rotation_mat[0,1])
+
+    # find the new width and height bounds
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    # rotate image with the new bounds and translated rotation matrix
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+
+
+
+
 def main(args):
     image_name = "/home/amir/modified_drone_ws/src/drone_racing/tiers_drone_racing/images/shapes2.jpg"
     img = cv2.imread(image_name)
+    img2 = cv2.imread(image_name)
     cv2.imshow("original",img)
+
+
 
 
 
@@ -70,25 +106,49 @@ def main(args):
     lower_band =(45, 100, 100)
     upper_band = (90, 255, 255)
     mask = cv2.inRange(hsv, lower_band, upper_band)
-    cv2.imshow("Mask",mask)
+    #cv2.imshow("Mask",mask)
 
     #get contour
     contours = getContours(mask)
 
     rgb_image, black_image =process_contours(mask, img, contours)
-    cv2.imshow("Edited_Photo",rgb_image)
+    #cv2.imshow("Edited_Photo",rgb_image)
     #cv2.imshow("Black Photo",black_image)
 
+    yaw = 35;
 
-    cv2.imshow("Original After",img)
+
+    backtorgb = cv2.cvtColor(mask,cv2.COLOR_GRAY2RGB)
+    height, width = backtorgb.shape[:2]
+    backtorgb[:,:,0]=np.ones((height, width))*255
+    backtorgb[np.where((backtorgb==[255,255,255]).all(axis=2))] = [0,0,255]
+    cv2.imshow("not compressed",backtorgb)
+
+
+    rotated_img=rotate_image(backtorgb, yaw)
+
+    percent = 20
+    dim = (int(rotated_img.shape[1]*percent/100), int(rotated_img.shape[0]*percent/100))
+    resized=cv2.resize(rotated_img, dsize=dim, interpolation =cv2.INTER_LINEAR)
+
+    cv2.imshow("Compressed",resized)
+
+
+
+
+    #cv2.imwrite('resized.png', resized)
+
+    #rotated_img=rotate_image(resized, 15)
+
+    #cv2.imshow("Original After",rotated_img)
 
     # Add Rectangular in middle
     #width = 50;
     #(rows,cols) = mask.shape
     #print ("Center is in {} , {} ".format(rows,cols))
     #img2	=	cv2.rectangle(mask, (width,width), (cols-width, rows- width), (255,255,255), -1)
-    img2 = Fill_the_center(mask,width=50)
-    cv2.imshow("Mask with rectangular",img2)
+    #img2 = Fill_the_center(mask,width=50)
+    #cv2.imshow("Mask with rectangular",img2)
 
     # Done!
     cv2.waitKey(0)
